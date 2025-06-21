e4c94c645aba058833a704d13c2fd0658095bf17
Lilan Manoj <lilanmanoj@gmail.com>
2025-06-21 17:53:15 +0530

    dev: Added context for favourites to share its state

--- app/_layout.js
+++ app/_layout.js
@@ -1,24 +1,28 @@
 import { Tabs } from 'expo-router';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { FavouritesProvider } from '../context/FavouritesContext';
 
 export default function RootLayout() {
   return (
-    <Tabs>
-        <Tabs.Screen
-            name="menu"
-            options={{
-                title: 'Menu',
-                tabBarIcon: ({ color }) => <MaterialIcons size={28} name="local-dining" color={color} />,
-                headerShown: false,
-            }}
-        />
-        <Tabs.Screen
-            name="favourites"
-            options={{
-                title: 'Favourites',
-                tabBarIcon: ({ color }) => <MaterialIcons size={28} name="favorite" color={color} />,
-            }}
-        />
-    </Tabs>
+    <FavouritesProvider>
+        <Tabs>
+            <Tabs.Screen
+                name="menu"
+                options={{
+                    title: 'Menu',
+                    tabBarIcon: ({ color }) => <MaterialIcons size={28} name="local-dining" color={color} />,
+                    headerShown: false,
+                }}
+            />
+            <Tabs.Screen
+                name="favourites"
+                options={{
+                    title: 'Favourites',
+                    tabBarIcon: ({ color }) => <MaterialIcons size={28} name="favorite" color={color} />,
+                    headerShown: false,
+                }}
+            />
+        </Tabs>
+    </FavouritesProvider>
   );
 }
